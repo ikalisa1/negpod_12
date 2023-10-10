65d43530358526f648d78338e4f387af6e17673a
John Ongeri <j.ouma@alustudent.com>
2023-10-10 02:32:50 -0700

    View all students saved in the file and list them on the terminal Delete the student in the file (By using the student ID)

--- group_12_summative/main.sh
+++ group_12_summative/main.sh
@@ -2,10 +2,26 @@
 
 # Function to create a student record
 create_student_record() {
-	    echo "Enter student email:"
-	        read email
-		    echo "Enter student age:"
-		        read age
-			    echo "Enter student ID:"
-			        read student_i 
-			}
+    echo "Enter student email:"
+    read email
+    echo "Enter student age:"
+    read age
+    echo "Enter student ID:"
+    read student_id
+
+    # Save the student record to a file
+    echo "$email $age $student_id" >> students-list_0923.txt
+    echo "Student record created and saved."
+}
+
+# Function to view all students
+view_all_students() {
+    echo "List of all students:"
+    cat students-list_0923.txt
+}
+
+# Function to delete a student by student ID
+delete_student() {
+    echo "Enter student ID to delete:"
+    read delete_id
+
